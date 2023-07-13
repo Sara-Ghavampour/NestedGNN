714ede6029e4bcd33d52d4f66f20216921d0730e
Arya-Ebrahimi <aryaebrahimi3000@mail.um.ac.ir>
2023-07-13 11:04:21 +0330

    scatter plot for 188 mutag graphs completed

--- kernel/gcn.py
+++ kernel/gcn.py
@@ -63,13 +63,13 @@ class NestedGCN(torch.nn.Module):
             xs += [x]
         x = global_mean_pool(torch.cat(xs, dim=1), data.node_to_subgraph)
         x = global_mean_pool(x, data.subgraph_to_graph)
-        features = F.relu(self.lin1(x))
-        x = F.dropout(features, p=0.5, training=self.training)
-        x = self.lin2(x)
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        features = self.lin2(x)
         if return_features:
-            return F.log_softmax(x, dim=-1),features
+            return F.log_softmax(features, dim=-1),features
         else:
-            return F.log_softmax(x, dim=-1)
+            return F.log_softmax(features, dim=-1)
 
     def __repr__(self):
         return self.__class__.__name__
--- temp.py
+++ temp.py
@@ -15,6 +15,14 @@ from torch_scatter import scatter_min
 from batch import Batch
 from collections import defaultdict
 from kernel.gcn import NestedGCN
+from torch_geometric.utils import to_undirected
+import matplotlib.pyplot as plt
+
+import numpy as np
+import scipy.sparse as sp
+# from bokeh.palettes import Category20_20, Category20b_20, Accent8
+from matplotlib import collections  
+
 
 from dataloader import DataLoader 
 from kernel.datasets import get_dataset
@@ -45,13 +53,28 @@ loss =0
 #     out = model(data)
 # loss += F.nll_loss(out, data.y.view(-1), reduction='sum').item()
 # print('loss: ',loss)
-
+dataset_embeddings=torch.empty((0,2), dtype=torch.float32)
+labels=torch.empty((0), dtype=torch.int32)
 for data in test_loader:
         data = data.to(device)
+        # print((data.y.view(-1)),type(data.y.view(-1)))
+        labels=torch.cat((labels,data.y.view(-1)),axis=0)
         with torch.no_grad():
-            out = model(data)
-        loss += F.nll_loss(out, data.y.view(-1), reduction='sum').item()
+            out,features = model(data,return_features=True)
+            print('features: ',features.shape)
+            dataset_embeddings=torch.cat((dataset_embeddings,features),axis=0)
+        loss += F.nll_loss(features, data.y.view(-1), reduction='sum').item()
+# dataset_embeddings = np.asarray(dataset_embeddings)
 print('loss: ',loss / len(test_loader.dataset))
+print('dataset_embeddings : ',dataset_embeddings.shape)
+
+print('1 data: ',dataset_embeddings[10,:])
+x = dataset_embeddings[:,0]
+y = dataset_embeddings[:,1]
+labels = labels.numpy()
+
+plt.scatter(x, y, alpha=0.5,c=labels)
+plt.show()
 # loss / len(loader.dataset)
 # model = NestedGCN(dataset, 4, 32, 'spd', False)
 # model.load_state_dict(torch.load('./models/nestedgcn2.pt'))
@@ -76,3 +99,12 @@ print('loss: ',loss / len(test_loader.dataset))
 
 # for i in range (0,len(list(model.parameters()))):
 #     print('i: ,',i,' ',((list(model.parameters())[i]).data))
+
+def build_adjacency_matrix(edge_index, num_nodes, is_directed=True):
+    if not is_directed:
+        edge_index = to_undirected(edge_index, num_nodes)
+
+    adj_matrix = torch.zeros((num_nodes, num_nodes))
+    adj_matrix[edge_index[0], edge_index[1]] = 1
+
+    return adj_matrix
